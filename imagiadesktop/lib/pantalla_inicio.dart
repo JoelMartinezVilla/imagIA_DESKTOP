fed1705a58a7711d552686ad6a1c7d22da7a761a
Komart21 <152431488+Komart21@users.noreply.github.com>
2025-01-22 19:57:52 +0100

    Pantalla inicio sesion
    Pantalla hecha, falta guardar datos y intoducir lo de la base de datos.

--- imagiadesktop/lib/pantalla_inicio.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-// Creació app Flutter. Per accedir a l'app cal introduir: URL del servidor, usuari (usuaris administradors). 
-//La URL del servidor es guardarà en un arxiu perquè aparegui directament quan s'obri de nou l'aplicació. 
-//Creame la interficie, que se llama pantalla_inicio.dart y quiero que me pongas 3 recuadros para poner la URL de servidor, 
-//otra para poner el usuario y otro para poner la contraseña y un boton para acceder, ponmelo bonito, 
-//que este ajustado en el medio uno debajo del otro y lo mismo con el boton, ajustado en el medio justo debajo de las demas cosas. Y lo del archivo, 
-//que se me guarde en un dades.json y que solamente se guarde la URL del servidor y el Usuario, la contraseña no hace falta.
